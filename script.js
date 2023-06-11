9cd7cd0e5b5b6ac8a1b9bc66f31c7023221d8622
global_gifts <fifa.1998.sava@mail.ru>
2023-06-11 20:38:12 +1000

    new js

---
+++ script.js
@@ -0,0 +1,70 @@
+//#1
+let a1 = 10;
+let b1 = 20;
+alert(a1)
+alert(b1)
+
+//#2
+let FirstPhone = 'Первый iPhone был представлен Стивом Джопсом 9 января 2007 года';
+alert(FirstPhone)
+
+//#3
+let FirstNameJS = 'Создателем языка JavaScript считается Брендан Эйх';
+alert(FirstNameJS)
+
+//#4
+let a4 = 10;
+let b4 = 2;
+let Sum = a4 + b4;
+alert(`10 + 2 = ${Sum}`)
+let subtraction = a4 - b4;
+alert(`10 - 2 = ${subtraction}`)
+let multiply = a4 * b4;
+alert(`10 * 2 = ${multiply}`)
+let div = a4 / b4;
+alert(`10 / 2 = ${div}`)
+
+//#5
+let a5 = 2;
+let b5 = 5;
+let result = a5 ** b5;
+alert(`2 ^ 5 = ${result}`)
+
+//#6
+let a6 = 9;
+let b6 = 2;
+let divRem = a6 % b6;
+alert(`Остаток от деления 9 на 2 = ${divRem}`)
+
+//#7
+let a7 = 1;
+a7 += 5;
+a7 -= 3;
+a7 *= 7;
+a7 /= 3;
+a7++;
+a7--;
+alert(a7);
+
+//#8
+let yourAge = prompt( 'Сколько вам лет?' )
+alert(`Вам ${yourAge} лет`)
+
+//#9.1
+let user = { 
+    name: 'Валерий', 
+    age: 27, 
+    isAdmin: true
+    };
+//#9.1
+user['city of residence'] = 'Красноярск'
+//#9.2
+user.age = user.weight;
+user.weight = 80;
+//#9.3
+delete user['city of residence'];
+//#9.4
+let info = prompt( 'Какую информацию хотите узнать о пользователе?' )
+alert( `Его имя ${user.name}`);
+alert( `Его вес ${user.weight} кг`);
+
