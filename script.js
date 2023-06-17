6795b1ad9a52dedb36c180d2c694b0fa30f03a6c
global_gifts <fifa.1998.sava@mail.ru>
2023-06-17 18:32:37 +1000

    new h3

--- script.js
+++ script.js
@@ -1,3 +1,144 @@
+//#3.1
+let password = 'пароль';
+let unlock = Number(prompt( `Введите пароль` ));
+
+if (unlock === 1111) {
+    alert(`Пароль введен верно`)
+}
+else {
+    alert(`Пароль введен неправильно`)
+}
+
+//#3.2
+let c = 5;
+
+if (c < 10 && c > 0) {
+    console.log(`Верно`)
+}
+else {
+    console.log(`Неверно`)
+}
+
+//#3.3
+let d = 3;
+let e = 10;
+
+if (d > 100 || e > 100) {
+    console.log(`Верно`)
+}
+else {
+    console.log(`Неверно`)
+}
+
+//#3.4
+let a = '2';
+let b = '3';
+a = Number(a);
+b = Number(b);
+alert( a + b);
+
+//#3.5
+let mothNumber = Number(prompt(`Введите номер месяца`));
+switch(mothNumber) {
+
+    case(12):
+    case(1):
+    case(2):
+    console.log(`Зимний месяц`)
+    break;
+
+    case(3):
+    case(4):
+    case(5):
+    console.log(`Весенний месяц`)
+    break;
+    
+    case(6):
+    case(7):
+    case(8):
+    console.log(`Летний месяц`)
+    break;
+
+    case(9):
+    case(10):
+    case(11):
+    console.log(`Осенний месяц`)
+    break;
+
+    default:
+    console.log('Такого месяца не существует!');
+    break;
+}
+
+//#3.6 Создать адаптивную версию сайта
+// <script src="script.js"></script> ДОБАВЛЕНИЕ JS
+
+//#3.7
+let even = Number(prompt(`Пожалуйста, введите любое число`));
+switch(even % 2) {
+    case(0):
+        alert(`Четное число`)
+        break;
+
+    case(1):
+        alert(`Нечетное число`)
+        break;
+
+    default:
+        alert(`Вы не ввели числовое значение!`)
+        break;
+}
+
+//#3.8
+
+let clientOs =Number(prompt(`Для установки мобильного приложения введите "1" - для Android или "0" - для iOS`));
+switch (clientOs) {
+    case (1):
+        alert(`Установите версию приложения для Android по ссылке`);
+        break;
+    
+    case (0):
+        alert(`Установите версию приложения для iOS по ссылке`);
+        break;
+        
+    default:
+        alert(`Неправильный ввод!`)
+        break;
+    }
+
+//#3.9
+let client = Number(prompt(`Для установки мобильного приложения введите: "1" - для Android или "0" - для iOS`));
+let clientDeviceYear = Number(prompt(`Далее определим год выпуска вашего устройства: "1" - ранее 2015 или "0" - позднее 2015`))
+
+switch(client && clientDeviceYear) {
+    case ( 1 && 1):
+        alert(`Установите облегченную версию приложения для Android по ссылке`)
+        break;
+
+    case ( 0 && 1):
+        alert(`Установите облегченную версию приложения для iOS по ссылке`)
+        break;
+
+    case ( 1 && 0):
+        alert(`Установите версию приложения для Android по ссылке`)
+        break;
+
+    case ( 0 && 0):
+        alert(`Установите версию приложения для iOS по ссылке`)
+        break;
+
+    default:
+        alert(`Неправильный ввод!`)
+        break;
+    }
+
+
+
+
+
+
+
+//------------------------------------------------------
 //#1
 let a1 = 10;
 let b1 = 20;
